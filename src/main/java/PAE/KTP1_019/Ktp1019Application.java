e9bd049819ba6547b079fd8935ed03a734963979
Feizal Septya <feizal.septya.ft19@mail.umy.ac.id>
2022-04-14 22:13:08 +0700

    first commit

---
+++ src/main/java/PAE/KTP1_019/Ktp1019Application.java
@@ -0,0 +1,13 @@
+package PAE.KTP1_019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ktp1019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ktp1019Application.class, args);
+	}
+
+}
---
+++ src/test/java/PAE/KTP1_019/Ktp1019ApplicationTests.java
@@ -0,0 +1,13 @@
+package PAE.KTP1_019;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ktp1019ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
