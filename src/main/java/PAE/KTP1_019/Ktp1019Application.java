d331d095cc76f840d5d3beacdfcf15e133d65542
Feizal Septya <feizal.septya.ft19@mail.umy.ac.id>
2022-06-10 12:41:11 +0700

    first commit

--- src/main/java/PAE/KTP1_019/Ktp1019Application.java
+++ src/main/java/PAE/KTP1_019/Ktp1019Application.java
@@ -2,12 +2,18 @@ package PAE.KTP1_019;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Ktp1019Application {
+public class Ktp1019Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ktp1019Application.class, args);
 	}
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Ktp1019Application.class);
+    }
 
 }
